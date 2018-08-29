ba732ce1215dac503573a4bf8194bafd4ac8e6b7
shuri <19960528shuri@gmail.com>
2018-08-29 18:20:30 +0900

    再提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :books
-get "/top" => "books#top"
+get "/" => "books#top"
 end
